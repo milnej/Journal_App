a75961ef1d7b44161beda9e81d34193898382f1e
James Milne <jamesmilne925@gmail.com>
2020-07-20 22:55:49 -0400

    vuex state manager basics implemented and can navigate to log in page

---
+++ client/src/views/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id='mainPage'>
+        <title-bar></title-bar>
+        <router-view/>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import TitleBar from '../components/TitleBar.vue'
+
+@Component({
+    components: {
+        TitleBar
+    }
+})
+export default class MainPage extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
