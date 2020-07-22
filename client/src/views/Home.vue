5ee62dcccdad9a8e98ffb54d873badf6bbd14b24
James Milne <jamesmilne925@gmail.com>
2020-07-21 21:49:47 -0400

    fixed navigate from login to home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,11 +7,21 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
+import { Getter, Action } from 'vuex-class'
+
+const namespace = 'profile'
 
 @Component({
     components: {}
 })
 export default class Home extends Vue {
+    
+    @Action('updateAccountStatus', { namespace })  updateAccountStatus!: Function
+
+    // lifecycle hooks
+    created() {
+        this.updateAccountStatus('false')
+    }
 
 }
 </script>
